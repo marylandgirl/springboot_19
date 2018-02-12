7ea2ddca64f20ef4ce8594a766d28badf9eb928e
Kim Levin <klevin5@student.umuc.edu>
2018-02-12 11:53:42 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot_19/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.springboot_19;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").access("hasAuthority('USER') or hasAuthority('ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").authorities("USER")
+        .and()
+        .withUser("dave").password("begreat").roles("ADMIN");
+    }
+}
---
+++ src/main/java/com/example/springboot_19/Springboot19Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot19Application.class, args);
+	}
+}
